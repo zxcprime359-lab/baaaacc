0459eed232ff2329c375a8a6e22cb20e83da1ed5
jerometecson-main <jerometecson.main@gmail.com>
2026-03-13 20:59:47 +0800

    xxx

--- hook/use-mobile.ts
+++ hook/use-mobile.ts
@@ -1,10 +1,10 @@
 import * as React from "react";
 
-const MOBILE_BREAKPOINT = 768;
+const MOBILE_BREAKPOINT = 640;
 
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean>(
-    window.innerWidth < MOBILE_BREAKPOINT
+    window.innerWidth < MOBILE_BREAKPOINT,
   );
 
   React.useEffect(() => {
